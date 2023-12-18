c748220c0c222da854e707d57bd9c5d218cdbfd9
Craig Shaffer <89813433+craigshaf50@users.noreply.github.com>
2023-12-17 18:26:44 -0600

    Update NFL_Weekly_Predictions.R
    More work summarizing weekly data. Need to figure out what next steps are, Want to be able to take most recent 5 weeks of data and use that along with betting lines to determine if a team covers the spread or not

--- Modeling/NFL_Weekly_Predictions.R
+++ Modeling/NFL_Weekly_Predictions.R
@@ -1,22 +1,42 @@
-#install.packages("tidyverse")
-#install.packages("nflverse")
-
+library(nflreadr)
 library(tidyverse)
-library(nflverse)
+library(gt)
+library(gtExtras)
+library(nflfastR)
 
+#load in participation PBP data & rosters
+pbp <- nflreadr::load_participation(2023, include_pbp = TRUE)
+schedule <- nflreadr::load_schedules(2023)
 
-### Historical Data ###
+#games
+games <- schedule %>%
+  select(season, week, away_team, home_team, away_score, home_score, result, spread_line, 
+         away_moneyline, home_moneyline, div_game)
 
-# Game data
-games_load<-load_schedules(2014:2022)
+#team stats and opposing team stats
+pos<-pbp %>%
+  group_by(season,week,posteam, defteam) %>%
+  filter(!is.na(posteam)) %>%
+  summarise(
+    team_EPAperPlay = mean(epa, na.rm = T),
+    team_QB_EPAperPlay = mean(qb_epa, na.rm = T),
+    team_PROE = mean(pass_oe, na.rm = T),
+    team_TotalPassYards = sum(passing_yards, na.rm = T),
+    team_TotalRushYards = sum(rushing_yards, na.rm = T),
+    team_PenalyYards = sum(penalty_yards, na.rm = T),
+    team_Drives = n_distinct(drive)
+  )
+opp<-pbp %>%
+  group_by(season,week,posteam, defteam) %>%
+  filter(!is.na(posteam)) %>%
+  summarise(
+    opp_EPAperPlay = mean(epa, na.rm = T),
+    opp_QB_EPAperPlay = mean(qb_epa, na.rm = T),
+    opp_PROE = mean(pass_oe, na.rm = T),
+    opp_TotalPassYards = sum(passing_yards, na.rm = T),
+    opp_TotalRushYards = sum(rushing_yards, na.rm = T),
+    opp_PenalyYards = sum(penalty_yards, na.rm = T),
+    opp_Drives = n_distinct(drive)
+  )
 
-games<-games_load %>% 
-  select(-c(5:7,12,15,16:24,28:29,31:32,35:39,42:46))
-
-
-
-
-
-# future & current
-games_2023<-load_schedules(2023) %>%
-  select(-c(5:7,12,15,16:24,28:29,31:32,35:39,42:46))
+weekly_stats<-left_join(pos, opp, by=join_by(x$season==y$season,x$week==y$week,x$defteam==y$posteam, x$posteam==y$defteam))
